273f13dfa0a4a6e4cc7b3875e62e077d91619f10
Mike <michael.schulman88@gmail.com>
2018-12-17 15:14:53 +0200

    First headline displayed

--- app.py
+++ app.py
@@ -1,20 +1,47 @@
 from bottle import route, static_file, request, run, get, error
 import feedparser
 import json
+from time import time
 
 
 feed = feedparser.parse("http://feeds.videogamer.com/rss/allupdates.xml")
+title1 = feed["entries"][0]["title"]
+links1 = feed["entries"][0]["link"]
 
+title_list = [title for title in feed["entries"]]
+print(title1)
+
+articles = [
+    {"title": title1 , "link": links1}
+]
 
 @route('/')
 def index():
-    return "Welcome to the Welcome Page. " \
-           "<a href='/test'>Next page</a>"
+    return static_file("index.html", root='')
+
+
+@route('/static/css/style.css')
+def css():
+    return static_file("style.css", root='static/css')
+
+
+@route('/static/js/logic.js')
+def js():
+    return static_file("logic.js", root='static/js')
+
+
+@route('/headlines', method='GET')
+def get_headlines():
+    return json.dumps(articles)
+
+
+@route('/images/<filename:re:.*\.(jpg|png)>', method='GET')
+def images(filename):
+    return static_file(filename, root='static/images')
 
 
 @error(404)
 def error404(error):
-    # return 'Nothing here, sorry'
     return static_file("404.html", root='')
 
 
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,1 +1,13 @@
-console.log("test")
+$.ajax({
+    type: "GET",
+    url: "http://localhost:7000/headlines", 
+}).done(function (data) {
+    $("#headlines").empty(); 
+    $("#headlines").append($("<ul/>")); 
+    var news = JSON.parse(data); 
+    news.forEach(function (element) {
+        console.log(element)
+        var newsItem = element.title + ", read more: " + element.links;
+        $("#headlines ul").append($("<li/>").html(newsItem)); 
+    });
+});
